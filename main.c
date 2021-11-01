95b0be20293a5bb2f34c1899e642c689ad7e2dcd
DamianMartinDominguez <64601914+DamianMartinDominguez@users.noreply.github.com>
2021-10-31 22:14:11 -0300

    2do parcial

---
+++ main.c
@@ -0,0 +1,114 @@
+
+/*
+legajo:102757   Dominguez Damian Email:d.dominguez@live.com.ar
+
+
+teoria:
+
+
+punto 1:
+
+1_ la cola es un tipo de organizacion donde el orden se va dando por la llegada del registro,
+la cola es del tipo primero en llegar , primero en salir. En cambio si bien la lista se puede organizar en base a
+alguna variable dentro del registro y no presisamente por el orden de llegada. ademas que puede ser doblemente enlazada
+para organizarse como el programador lo requiera.
+
+
+
+punto 2:
+
+2_ los comandos ddl son los referidos a la escritura de las bases de datos.
+desntro de sql tenemos comandos como RENAME(renombrar un dato u objeto), TRUNCATE(eliminar datos o registros),
+CREATE(crear datos, o registros, tablas, etc).
+
+
+
+practica:
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+/*
+
+1) Existe un archivo binario, “cliente.dat”, secuencial con la siguiente estructura:
+int id, char n[30] , unsigned int tipo, int b(baja lógica b=0), que son los datos de un cliente,
+donde cada bit del campo tipo representa alguna situación del cliente: el bit 5 en 1 significa que no tiene crédito
+y el bit 4 en 1 que es deudor, etc.
+ Crear una función que arme una pila con los registros que no sean deudores verificando que estén activos.
+Realizar el main() con la llamada a dicha función y crear un archivo con los datos de la pila. No utilizar variables Globales
+Enviar a Gustavo Viard: gviard_electronica@yahoo.com.ar
+*/
+
+typedef struct {
+ int id;
+ char n[30];
+ insigned int tipo;
+ int b;
+}datos_t;
+struct pila{
+struct datos d;
+struct pila *l;
+}pila_t;
+
+void setPila(struct pila_t);
+
+int main()
+ {
+ 
+  struct pila *pila= NULL, *aux= NULL;
+  void setpila(&p);
+ 
+ 
+  aux=(struct pila_t)malloc(sizeof(struct pila_t));
+ 
+  if(aux==NULL)
+  {
+  printf("\n no hay memoria disponible \n");
+  return 0;
+}
+  setPila(&pila);
+  while(pila)
+  {
+  printf("\n%d %s %d %d\n",pila->id,pila->n,pila->)tipo,pila->b);
+  aux=pila;
+  pila=pila->1;
+  free(aux);
+}
+ 
+return 0;
+}
+
+
+void setPila(struct pila_t)
+{
+struct datos pl;
+struct pila *aux;
+FILE *fp;
+if((fp=fopen("paciente.dat","rb"))==NULL)
+{
+printf("\n error en la lectura \n");
+  return 0;
+}
+fread(&pl,sizeof(datos_t),1,fp);
+while(!feof(fp))
+{
+if(pl.b!=0)
+{
+if((pl.tipo & 0x10)==0x00) // busco bit 4 en cero 0001 0000
+{
+aux=(struct pila*) malloc(sizeof(struct pila));
+if(aux)
+{
+aux= pila;
+pila= pila->l;
+pila=aux;
+}
+     
+}
+}
+}
+
+
+
+//fin de parcial
